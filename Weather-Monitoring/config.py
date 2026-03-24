6622d43e30fc57baea0b9eb93278c8925dce4474
Thirukkovela Sai Vaishnavi <145593705+vaishnavisai03@users.noreply.github.com>
2026-03-24 23:43:44 +0530

    Create config.py

---
+++ Weather-Monitoring/config.py
@@ -0,0 +1,5 @@
+
+API_KEY = 'YOUR_API_KEY'  # Replace with your actual API key
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+CITIES = ["Delhi", "Mumbai", "Chennai", "Bangalore", "Kolkata", "Hyderabad"]
+ALERT_THRESHOLD_TEMP = 35.0  # User-defined alert threshold
