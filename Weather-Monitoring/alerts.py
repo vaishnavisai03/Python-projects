384b467a49bd38cd82fc0d85bbccda043db3c900
Thirukkovela Sai Vaishnavi <145593705+vaishnavisai03@users.noreply.github.com>
2026-03-24 23:42:33 +0530

    Update alerts.py

--- Weather-Monitoring/alerts.py
+++ Weather-Monitoring/alerts.py
@@ -1,1 +1,17 @@
 
+from .database import connect_db
+from .config import ALERT_THRESHOLD_TEMP
+
+def check_alerts():
+    conn = connect_db()
+    c = conn.cursor()
+    c.execute('''SELECT city, temperature, weather_main, timestamp 
+                 FROM weather ORDER BY timestamp DESC LIMIT 6''')
+    latest_data = c.fetchall()
+    
+    for data in latest_data:
+        city, temperature, weather_main, timestamp = data
+        if temperature > ALERT_THRESHOLD_TEMP:
+            print(f"ALERT: {city} exceeded {ALERT_THRESHOLD_TEMP}°C with {temperature}°C")
+    
+    conn.close()
