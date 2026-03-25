34457defad95842c55b459591c3a6ef262cc2e89
Thirukkovela Sai Vaishnavi <145593705+vaishnavisai03@users.noreply.github.com>
2026-03-25 22:09:00 +0530

    Create fetcher.py

---
+++ Weather-Monitoring/fetcher.py
@@ -0,0 +1,11 @@
+
+from .api import fetch_weather
+from .database import store_weather
+from .config import CITIES
+
+def fetch_and_store_weather():
+    for city in CITIES:
+        weather_data = fetch_weather(city)
+        if weather_data:
+            store_weather(weather_data)
+    print("Data fetched and stored.")
